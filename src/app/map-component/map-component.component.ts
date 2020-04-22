0fdb7a66315a19f1d88dbc02eea05c1c9044591d
Tailon <renewedT2016@gmail.com>
2020-04-21 18:55:46 -0800

    Added map component and styling frame

---
+++ src/app/map-component/map-component.component.ts
@@ -0,0 +1,30 @@
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
+@Component({
+  selector: 'app-map-component',
+  templateUrl: './map-component.component.html',
+  styleUrls: ['./map-component.component.css']
+})
+export class MapComponentComponent implements AfterViewInit {
+  private map;
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 39.8282, -98.5795 ],
+      zoom: 3
+    });
+  }
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
+
+}
