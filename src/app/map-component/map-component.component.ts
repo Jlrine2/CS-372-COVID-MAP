4782db33b12ac43ea64d82f40f0fa9879a67f2e2
Jlrine2 <Jlrine2@alaska.edu>
2020-04-22 14:41:48 -0800

    moved points to center of states

--- src/app/map-component/map-component.component.ts
+++ src/app/map-component/map-component.component.ts
@@ -1,5 +1,23 @@
 import { Component, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
+import { icon, Marker} from 'leaflet';
+import { MarkerService } from '../services/marker.service';
+
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+L.Marker.prototype.options.icon = iconDefault;
+
 @Component({
   selector: 'app-map-component',
   templateUrl: './map-component.component.html',
@@ -15,10 +33,13 @@ export class MapComponentComponent implements AfterViewInit {
     });
   }
 
-  constructor() { }
+  constructor(private  markerService: MarkerService) { }
 
   ngAfterViewInit(): void {
     this.initMap();
+    this.markerService.makeCapitalMarkers(this.map);
+
+
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
