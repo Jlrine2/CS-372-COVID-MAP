a78c6935a29a3227d1e79727788e8e36a7da659a
Jlrine2 <Jlrine2@alaska.edu>
2020-04-24 17:42:22 -0800

    adjusting size of circles

--- src/app/services/marker.service.ts
+++ src/app/services/marker.service.ts
@@ -9,16 +9,17 @@ import * as L from 'leaflet';
 export class MarkerService {
   capitals: string = '/assets/capitals.geojson';
   private data: {};
+  private metresPerPixel: number;
   constructor(private http: HttpClient, private dataService: DataService) {
   }
   makeCapitalMarkers(map: L.map): void {
     this.data = this.dataService.getData();
-    console.log(this.data);
     this.http.get(this.capitals).subscribe((res: any) => {
       for (const c of res.features) {
         const lat = c.geometry.coordinates[0];
         const lon = c.geometry.coordinates[1];
-        const marker = L.circleMarker([lon, lat], { radius: Math.log2(this.data[c.properties.state]) , color: '#ff1515'}).addTo(map);
+        console.log(this.data[c.properties.state]);
+        const marker = L.circle([lon, lat], { radius: (this.data[c.properties.state]), color: '#ff1515'}).addTo(map);
       }
     });
   }
