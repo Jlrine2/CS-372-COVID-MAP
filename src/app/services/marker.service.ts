4378d30a29a6bb461ab313b418497c89121131db
Jlrine2 <Jlrine2@alaska.edu>
2020-04-29 14:18:59 -0800

    move call to get data out of make markers

--- src/app/services/marker.service.ts
+++ src/app/services/marker.service.ts
@@ -8,18 +8,17 @@ import * as L from 'leaflet';
 })
 export class MarkerService {
   capitals: string = '/assets/capitals.geojson';
-  private data;
+  private data = this.dataService.getData();
   private metresPerPixel: number;
   constructor(private http: HttpClient, private dataService: DataService) {
   }
   makeCapitalMarkers(map: L.map): void {
-    this.data = this.dataService.getData();
     this.http.get(this.capitals).subscribe((res: any) => {
       for (const c of res.features) {
         const lat = c.geometry.coordinates[0];
         const lon = c.geometry.coordinates[1];
         console.log(this.data);
-        const marker = L.circle([lon, lat], { radius: this.data[c.properties.state], color: '#ff1515'}).addTo(map);
+        L.circle([lon, lat], { radius: this.data[c.properties.state], color: '#ff1515'}).addTo(map);
       }
     });
   }
